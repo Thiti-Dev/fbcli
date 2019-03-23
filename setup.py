92dd8929bf69d59f339058df29819197c9fa130f
Chinmaya Krishnan Mahesh <cmahesh2@illinois.edu>
2019-03-23 16:30:09 -0500

    Add pypi requirements

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fb-cli-chin123",
+    version="0.0.1",
+    author="Chinmaya Krishnan Mahesh",
+    author_email="chinmaya.mahesh@gmail.com",
+    description="Facebook Command Line Client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chin123/fbcli",
+    scripts=['fb-cli'],
+    install_requires=['bs4', 'requests', 'colorama'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
